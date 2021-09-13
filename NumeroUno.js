98b71e87b5231f20695133f1a37dfae42be673f4
Snaksiu <72550913+Snaksiu@users.noreply.github.com>
2021-09-13 15:14:18 +0200

    znowu kolejnuuuuuuu

--- NumeroUno.js
+++ NumeroUno.js
@@ -15,3 +15,7 @@ console.log('The area of a square with a width of 4 is ' + square.area(4));
 console.log('First');
 console.log('Second');
 
+setTimeout(function() {
+    console.log('First');
+    }, 3000);
+ console.log('Second');
